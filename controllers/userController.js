521ab54bfcbb69af25a6de75ad46eda7808db5c1
Ayush <ayush3032@gmail.com>
2022-10-03 00:57:15 +0530

    add signup page

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,3 +4,10 @@ module.exports.render_login_page = function(req, res) {
         title: "login Page"
     });
 }
+
+module.exports.render_signup_page = function(req, res) {
+
+    return res.render('signUp', {
+        title: "Signup Page"
+    });
+}
--- router/user.js
+++ router/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/login_page', userController.render_login_page);
+router.get('/signup_page', userController.render_signup_page);
 
 module.exports = router;
