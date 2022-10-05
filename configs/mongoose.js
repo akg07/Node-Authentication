3e1c0b8fd9f0367956b0a350d06d59c0668024aa
Ayush <ayush3032@gmail.com>
2022-10-06 01:36:32 +0530

    add comment in configs

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,14 +1,26 @@
+/*
+    This Config is written by Aayush Kumar Gupta
+    This is config file for mongoDB
+
+*/
+
+// get instance of mongoose
 const mongoose = require('mongoose');
 
-
+// connect to a database
 mongoose.connect(`mongodb://localhost/node_auth`);
 
+// create a connection
 const db = mongoose.connection;
 
+// in case of error 
 db.on('error', console.error.bind(console, 'Error Connecting to mongo db'));
 
+// if conneciton established
 db.once('open', function(){
     console.log('Connected to MongoDB Database :: node_auth');
 });
 
+
+// export db instance
 module.exports = db;
